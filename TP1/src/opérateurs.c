7aa37c3adc5460c29faae02675c511ae258653a8
cm2025ma-cmyk <cholinmatthieu@gmail.com>
2026-01-29 11:20:07 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,14 @@
+#include <stdio.h>
+int main() {
+  int a = 5;
+  int b = 10;
+  printf("Le resultat est %d\n", a + b);
+  printf("Le résultat de la SOUSTRACTION est %d\n", b-a);
+  printf("Le résultat de la Multiplication est %d\n", a*b);
+  printf("Le résultat de la division est %d\n", a/b);
+  printf("Le reste de la division est %d\n", a%b);
+bool sont_egaux(int a, int b) {
+    return (a == b);
+} 
 
 
