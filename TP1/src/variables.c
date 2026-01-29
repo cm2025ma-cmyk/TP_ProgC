2dfe5f95739454b37ec3c29f2d36433256e67275
cm2025ma-cmyk <cholinmatthieu@gmail.com>
2026-01-29 10:54:59 +0100

    Update variables.c

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -2,17 +2,14 @@
 
 int main() {
     // --- 1. CHAR (Caractères et petits entiers) ---
-    // %c pour le caractère, %d pour sa valeur numérique
     signed char c_signe = 'A';
-    unsigned char c_non_signe = 200; // 200 dépasse la capacité d'un signed char (-128 à 127)
+    unsigned char c_non_signe = 200; 
 
     printf("--- CHAR ---\n");
     printf("Signed char (lettre)   : %c (valeur num: %d)\n", c_signe, c_signe);
     printf("Unsigned char (valeur) : %u\n", c_non_signe);
 
-
     // --- 2. SHORT (Entiers courts) ---
-    // %hd pour short, %hu pour unsigned short
     short s_signe = -12345;
     unsigned short s_non_signe = 60000;
 
@@ -20,22 +17,39 @@ int main() {
     printf("Signed short           : %hd\n", s_signe);
     printf("Unsigned short         : %hu\n", s_non_signe);
 
-
     // --- 3. INT (Entiers standards) ---
-    // %d pour int, %u pour unsigned int
     int i_signe = -1000000;
-    unsigned int i_non_signe = 3000000000U; // Le suffixe 'U' indique que c'est Unsigned
+    unsigned int i_non_signe = 3000000000U; 
 
     printf("\n--- INT ---\n");
     printf("Signed int             : %d\n", i_signe);
     printf("Unsigned int           : %u\n", i_non_signe);
 
-
     // --- 4. LONG INT (Entiers longs) ---
-    // %ld pour long, %lu pour unsigned long
-    long int l_signe = -1234567890L; // Le suffixe 'L' indique Long
+    long int l_signe = -1234567890L; 
     unsigned long int l_non_signe = 4000000000UL;
 
     printf("\n--- LONG INT ---\n");
     printf("Signed long            : %ld\n", l_signe);
     printf("Unsigned long          : %lu\n", l_non_signe);
+
+    // --- 5. LONG LONG INT (Entiers très longs) ---
+    long long int ll_signe = -9000000000000000000LL; 
+    unsigned long long int ll_non_signe = 18000000000000000000ULL; 
+
+    printf("\n--- LONG LONG INT ---\n");
+    printf("Signed long long       : %lld\n", ll_signe);
+    printf("Unsigned long long     : %llu\n", ll_non_signe);
+
+    // --- 6, 7, 8. FLOTTANTS (Nombres à virgule) ---
+    float f = 3.141592f; 
+    double d = 3.141592653589793;
+    long double ld = 3.141592653589793238462643L; 
+
+    printf("\n--- FLOTTANTS ---\n");
+    printf("Float                  : %.10f\n", f);
+    printf("Double                 : %.15lf\n", d);
+    printf("Long Double            : %.18Lf\n", ld); 
+
+    return 0;
+}
